88add734e841c2680366344a7be39dda1cf349ba
Alexey Ahremenko <a.ahremenko@gmail.com>
2020-07-25 16:45:58 +0300

    Задача 2.2 - рефакторинг по замечаниям + использован код из правильного решения

--- src/main/by/htp/ahremenko/task22/service/AnagramService.java
+++ src/main/by/htp/ahremenko/task22/service/AnagramService.java
@@ -1,17 +1,16 @@
 package by.htp.ahremenko.task22.service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.sun.deploy.util.StringUtils;
+import lombok.experimental.UtilityClass;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 2.2 Анаграммы
  * Анаграммы – это слова, которые состоят из одних и тех же символов, но в разном порядке.
  * Дан массив строк, найти в нем анаграммы и вернуть список групп этих анаграмм.
  * Каждый список отсортирован в алфавитном порядке. Оценить сложность написанного алгоритма.
- * <p>
  * Пример:
  * ["eat", "tea", "tan", "ate", "nat", "bat"]
  * На выходе должны получить:
@@ -25,60 +24,21 @@ import java.util.Map;
  * в исходном массиве, ["nat","tan"] второй, потому что “tan” было найдено вторым
  * (вообще третьим, но т.к. “tea” вошло в первую группу анаграмм, то считается вторым) и т.д.
  */
-public final class AnagramService {
-
-    private AnagramService() {
-    }
+@UtilityClass
+public class AnagramService {
 
     public static List<List<String>> handle(List<String> list) {
-        List<List<String>> result = new ArrayList<>();
+        Map<String,List<String>> result = new LinkedHashMap<>();
 
-        for (int i = 0; i < list.size(); i++) {
-            Integer found = null;
-            for (int j = 0; j < result.size(); j++) {
-                if (isAnagram(list.get(i), result.get(j).get(0))) {
-                    found = j;
-                    break;
-                }
-            }
-            if (found != null) {
-                result.get(found).add(list.get(i));
-            } else {
-                List<String> anagrams = new ArrayList<>();
-                anagrams.add(list.get(i));
-                result.add(anagrams);
-            }
+        for (String word : list) {
+            char[] letters = word.toLowerCase().replaceAll("[\\s]", "").toCharArray();
+            Arrays.sort(letters);
+            String mapKey = new String(letters);
+            List<String> mapValue = result.computeIfAbsent(mapKey, k -> new ArrayList<>());
+            mapValue.add(word);
         }
-        result.forEach(a -> Collections.sort(a));
-        return result;
-    }
-
-    public static boolean isAnagram(String firstWord, String secondWord) {
-        char[] word1 = firstWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
-        char[] word2 = secondWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
-        Map<Character, Integer> lettersMap = new HashMap<>();
-
-        for (char c : word1) {
-            int count = 1;
-            if (lettersMap.containsKey(c)) {
-                count = lettersMap.get(c) + 1;
-            }
-            lettersMap.put(c, count);
-        }
-
-        for (char c : word2) {
-            int count = -1;
-            if (lettersMap.containsKey(c)) {
-                count = lettersMap.get(c) - 1;
-            }
-            lettersMap.put(c, count);
-        }
-
-        for (char c : lettersMap.keySet()) {
-            if (lettersMap.get(c) != 0) {
-                return false;
-            }
-        }
-        return true;
+        return result.values().stream()
+                .peek(lst -> lst.sort(Comparator.naturalOrder()))
+                .collect(Collectors.toList());
     }
 }
